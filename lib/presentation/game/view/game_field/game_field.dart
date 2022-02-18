444f397ab11863f0c21214bec841811439e950b3
nukeolay <66204571+nukeolay@users.noreply.github.com>
2022-02-18 11:35:53 +0300

    implemented flips and reset mvvm

--- lib/presentation/game/view/game_field/game_field.dart
+++ lib/presentation/game/view/game_field/game_field.dart
@@ -1,29 +1,30 @@
-import 'package:darkit/domain/levels/entities/cell_entity.dart';
-import 'package:darkit/old_realisation/game/widgets/cell_widget.dart';
-import 'package:darkit/old_realisation/game/widgets/disable_multitouch.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:darkit/presentation/game/view/game_field/widgets/cell_widget.dart';
+import 'package:darkit/presentation/game/view/game_field/widgets/disable_multitouch.dart';
+import 'package:darkit/presentation/game/view_model/view_model.dart';
+import 'package:darkit/presentation/game/view_model/view_model_state.dart';
 
 class GameField extends StatelessWidget {
   const GameField({
     Key? key,
-    // required this.cells,
-    // required this.length,
-    // required this.cellWidth,
   }) : super(key: key);
-
-  final List<CellEntity> cells = const [];
-  final int length = 0;
-  final double cellWidth = 0;
 
   @override
   Widget build(BuildContext context) {
+    final GameViewModel _viewModel = context.watch<GameViewModel>();
+    final GameViewModelState _state = _viewModel.state;
+    final double width = MediaQuery.of(context).size.width;
+    final double cellWidth = width / _state.fieldLength - 10;
+
     return DisableMultitouchWidget(
       child: GridView.builder(
         physics: const BouncingScrollPhysics(),
-        itemCount: cells.length,
+        itemCount: _state.cells.length,
         shrinkWrap: true,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: length,
+          crossAxisCount: _state.fieldLength,
           crossAxisSpacing: 0,
           mainAxisSpacing: 0,
         ),
@@ -32,7 +33,7 @@ class GameField extends StatelessWidget {
           child: SizedBox(
             width: cellWidth,
             height: cellWidth,
-            child: CellWidget(cells[index]),
+            child: CellWidget(index)
           ),
         ),
       ),
